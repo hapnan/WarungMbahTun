0cd7ae64f25812dd47f2db96964c85f23a7149e5
hapnan <hapnanarsad@gmail.com>
2017-11-29 23:57:26 +0700

    Initial commit

---
+++ app/src/main/java/slg/dev/warungmbahtun/home.java
@@ -0,0 +1,16 @@
+package slg.dev.warungmbahtun;
+
+import android.content.Intent;
+import android.net.wifi.hotspot2.pps.HomeSp;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
